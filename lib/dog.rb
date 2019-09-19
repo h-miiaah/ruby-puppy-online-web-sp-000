d6af852830e7f70745b466d06de8d874e50a11ea
Haim D <haim11293@aol.com>
2019-09-19 05:48:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,13 @@
 # Add your code here
+class Dog
+
+  @@all = [] # set a class variable @@all equal to an empty array to store all dogs created.
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+end
